c8c6366572c93b7be029b83572d00c96c19dd338
Luiz Felipe Correa <89542491+lipe22cwb@users.noreply.github.com>
2026-09-14 04:55:49 -0600

    Complete Week 2 assignment

---
+++ app/week-2/page.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import StudentInfo from "./student-info";
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Shopping List</h1>
+
+      <StudentInfo />
+
+      <Link href="/">← Back to Home</Link>
+    </main>
+  );
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Luiz Felipe Correa</p>
+      <p>
+        My GitHub:{" "}
+        <Link href="https://github.com/lipe22cwb/cprg306-assignments">
+          lipe22cwb
+        </Link>
+      </p>
+    </div>
+  );
+}
