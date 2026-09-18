519e3f0edd0b623d9c0b0c378eb780674a3abe85
Louis <71915817+louispadillo@users.noreply.github.com>
2026-09-18 15:37:17 -0600

    added farbod's github and name

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -11,6 +11,10 @@ export default function StudentInfo() {
       <Link href="https://github.com/louispadillo">
         My Github: louispadillo
       </Link>
+      <h1>Name: Farbod Rahimzadeh</h1>
+      <Link href="https://github.com/Farbod-sait">
+        My Github: farbod-sait
+      </Link>
     </div>
   );
 }
